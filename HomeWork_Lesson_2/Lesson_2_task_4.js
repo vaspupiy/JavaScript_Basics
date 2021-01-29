575d4f0aed1f08ea5925df3da051e51f17c343d5
Vasiliy <vaspupiy@mail.ru>
2021-01-29 21:32:49 +0300

    lesson_2_working_on_bugs

--- HomeWork_Lesson_2/Lesson_2_task_4.js
+++ HomeWork_Lesson_2/Lesson_2_task_4.js
@@ -3,61 +3,47 @@
 // 4. Присвоить переменной а значение в промежутке [0..15].
 // С помощью оператора switch организовать вывод чисел от a до 15.
 
-alert(' 4. Присвоить переменной а значение в промежутке [0..15].\n' +
-    ' С помощью оператора switch организовать вывод чисел от a до 15.');
-
 function getRandomIntMinMax(minNum, maxNum) {
     return Math.floor(Math.random() * (maxNum - minNum)) + minNum;  //max не вкл. min вкл.
 }
 
-function concatFoo(ser, num) {
-    return ser + ' ' + num;
-}
-
-function fromAndTo(number) {
-    let series = '';
-    switch (number) {
-        case 0:
-            series = concatFoo(series, number++);
-        case 1:
-            series = concatFoo(series, number++);
-        case 2:
-            series = concatFoo(series, number++);
-        case 3:
-            series = concatFoo(series, number++);
-        case 4:
-            series = concatFoo(series, number++);
-        case 5:
-            series = concatFoo(series, number++);
-        case 6:
-            series = concatFoo(series, number++);
-        case 7:
-            series = concatFoo(series, number++);
-        case 8:
-            series = concatFoo(series, number++);
-        case 9:
-            series = concatFoo(series, number++);
-        case 10:
-            series = concatFoo(series, number++);
-        case 11:
-            series = concatFoo(series, number++);
-        case 12:
-            series = concatFoo(series, number++);
-        case 13:
-            series = concatFoo(series, number++);
-        case 14:
-            series = concatFoo(series, number++);
-        case 15:
-            return concatFoo(series, number++).trim();
-    }
-    return null;
-}  // надеюсь не сильно набыдлокодил...
-
 const MIN = 0;
 const MAX = 15;
 
-let a = getRandomIntMinMax(MIN, MAX + 1);
-// console.log(a);
-// console.log(fromAndTo(a));
+let number = getRandomIntMinMax(MIN, MAX + 1);
+console.log(`random number: ${number}`);
 
-alert('a = ' + a + ' результат ' + fromAndTo(a));
+switch (number) {
+    case 0:
+        console.log(number++);
+    case 1:
+        console.log(number++);
+    case 2:
+        console.log(number++);
+    case 3:
+        console.log(number++);
+    case 4:
+        console.log(number++);
+    case 5:
+        console.log(number++);
+    case 6:
+        console.log(number++);
+    case 7:
+        console.log(number++);
+    case 8:
+        console.log(number++);
+    case 9:
+        console.log(number++);
+    case 10:
+        console.log(number++);
+    case 11:
+        console.log(number++);
+    case 12:
+        console.log(number++);
+    case 13:
+        console.log(number++);
+    case 14:
+        console.log(number++);
+    case 15:
+        console.log(number)
+}
