6f32ff2653c3d4bb44adefa7b9c5c6b0e6d86f1e
Vasiliy <vaspupiy@mail.ru>
2021-02-13 18:07:37 +0300

    homeWork_lesson_7

---
+++ home_work_lesson_7/lesson_7.js
@@ -0,0 +1,435 @@
+"use strict";
+const settings = {
+    rowsCount: 21,
+    colsCount: 21,
+    speed: 2,
+    barrierCount: 3,
+    winFoodCount: 5,
+};
+
+const config = {
+    settings,
+    init(userSettings) {
+        Object.assign(this.settings, userSettings);
+    },
+
+    getRowsCount() {
+        return this.settings.rowsCount;
+    },
+
+    getColsCount() {
+        return this.settings.colsCount;
+    },
+
+    getSpeed() {
+        return this.settings.speed;
+    },
+
+    getWinFoodCount() {
+        return this.settings.winFoodCount;
+    },
+
+    validate() {
+        const result = {
+            isValid: true,
+            errors: [],
+        };
+
+        if (this.getRowsCount() < 10 || this.getRowsCount() > 30) {
+            result.isValid = false;
+            result.errors.push('Неверные настройки, значение должно быть 10 > rowsCount < 30');
+        }
+
+        if (this.getColsCount() < 10 || this.getColsCount() > 30) {
+            result.isValid = false;
+            result.errors.push('Неверные настройки, значение должно быть 10 > colsCount < 30');
+        }
+
+        if (this.getSpeed() < 1 || this.getSpeed() > 10) {
+            result.isValid = false;
+            result.errors.push('Неверные настройки, значение должно быть 1 > speed < 10');
+        }
+
+        if (this.getWinFoodCount() < 5 || this.getWinFoodCount() > 50) {
+            result.isValid = false;
+            result.errors.push('Неверные настройки, значение должно быть 5 > winFoodCount < 50')
+        }
+
+        return result
+    },
+};
+
+const map = {
+    cells: null,
+    usedCells: [],
+
+    init(rowsCount, colsCount) {
+        const table = document.getElementById('game');
+        table.innerHTML = '';
+
+        this.cells = {};
+        this.userCells = [];
+
+        for (let row = 0; row < rowsCount; row++) {
+            const tr = document.createElement('tr');
+            tr.classList.add('row');
+            table.appendChild(tr);
+
+            for (let col = 0; col < colsCount; col++) {
+                const td = document.createElement('td');
+                td.classList.add('cell');
+
+                this.cells[`x${col}_y${row}`] = td;
+                tr.appendChild((td))
+            }
+        }
+    },
+
+    render(snakePointArray, foodPoint, barrierPoint) {
+        for (const cell of this.usedCells) {
+            cell.className = 'cell';
+        }
+
+        this.usedCells = [];
+
+        snakePointArray.forEach((point, index) => {
+            const snakeCell = this.cells[`x${point.x}_y${point.y}`];
+            snakeCell.classList.add(index === 0 ? 'snakeHead' : 'snakeBody');
+            this.usedCells.push(snakeCell);
+        });
+
+        const foodCell = this.cells[`x${foodPoint.x}_y${foodPoint.y}`];
+        foodCell.classList.add('food');
+        this.usedCells.push(foodCell);
+
+        const barrierCell = this.cells[`x${barrierPoint.x}_y${barrierPoint.y}`];
+        barrierCell.classList.add('barrier');
+        this.usedCells.push(barrierCell)
+    },
+};
+
+const snake = {
+    body: [],
+    direction: null,
+    lastStepDirection: null,
+
+    init(startBody, direction) {
+        this.body = startBody;
+        this.direction = direction;
+        this.lastStepDirection = direction;
+    },
+
+    getBody() {
+        return this.body;
+    },
+
+    getLastStepDirection() {
+        return this.lastStepDirection;
+    },
+
+    isOnPoint(point) {
+        return this.body.some(snakePoint => snakePoint.x === point.x && snakePoint.y === point.y);
+    },
+
+    makeStep() {
+        this.lastStepDirection = this.direction;
+        this.body.unshift(this.getNextStepHeadPoint());
+        this.body.pop();
+    },
+
+    growUp() {
+        const lastBodyIndex = this.body.length - 1;
+        const lastBodyPoint = this.body[lastBodyIndex];
+        const lastBodyPointClone = Object.assign({}, lastBodyPoint);
+        this.body.push(lastBodyPointClone);
+    },
+
+    getNextStepHeadPoint() {
+        const firstPoint = this.body[0];
+
+        switch (this.direction) {
+            case 'up':
+                return {x: firstPoint.x, y: firstPoint.y - 1};
+            case 'right':
+                return {x: firstPoint.x + 1, y: firstPoint.y};
+            case 'down':
+                return {x: firstPoint.x, y: firstPoint.y + 1};
+            case 'left':
+                return {x: firstPoint.x - 1, y: firstPoint.y};
+        }
+    },
+
+    setDirection(direction) {
+        this.direction = direction;
+    },
+};
+
+const food = {
+    x: null,
+    y: null,
+
+    getCoordinates() {
+        return {
+            x: this.x,
+            y: this.y,
+        };
+    },
+
+    setCoordinates(point) {
+        this.x = point.x;
+        this.y = point.y;
+    },
+
+    isOnPoint(point) {
+        return this.x === point.x && this.y === point.y;
+    }
+};
+
+const barrier = {
+    x: null,
+    y: null,
+
+    getCoordinates() {
+        return {
+            x: this.x,
+            y: this.y,
+        };
+    },
+
+    setCoordinates(point) {
+        this.x = point.x;
+        this.y = point.y
+    },
+
+    isOnPoint(point) {
+        return this.x === point.x && this.y === point.y;
+    }
+}
+
+const status = {
+    condition: null,
+
+    setPlaying() {
+        this.condition = 'playing';
+    },
+
+    setStopped() {
+        this.condition = 'stopped';
+    },
+
+    setFinished() {
+        this.condition = 'finished';
+    },
+
+    isPlaying() {
+        return this.condition === 'playing';
+    },
+
+    isStopped() {
+        return this.condition === 'stopped'
+    },
+};
+
+const score = {
+    total: null,
+
+    setTotal(value) {
+        this.total = value
+    },
+    getTotal() {
+        return this.total
+    },
+
+    render() {
+        const totalValue = document.querySelector('.score-area')
+        totalValue.innerHTML = this.getTotal();
+    }
+};
+
+const game = {
+    config,
+    map,
+    snake,
+    food,
+    status,
+    score,
+    barrier,
+    tickInterval: null,
+
+    init(userSettings) {
+        this.config.init(userSettings);
+        const validation = this.config.validate();
+
+        if (!validation.isValid) {
+            for (const err of validation.errors) {
+                console.log(err);
+            }
+            return;
+        }
+
+        this.map.init(this.config.getRowsCount(), this.config.getColsCount());
+        this.setEventHandlers();
+        this.reset();
+    },
+
+    reset() {
+        this.stop();
+        this.snake.init(this.getStartSnakeBody(), 'up');
+        this.score.setTotal(this.snake.body.length - 1)
+        this.food.setCoordinates(this.getRandomFreeCoordinates());
+        this.barrier.setCoordinates(this.getRandomFreeCoordinates());
+        console.log(barrier)
+        this.render();
+    },
+
+    play() {
+        this.status.setPlaying();
+        this.tickInterval = setInterval(() => this.tickHandler(), 1000 / this.config.getSpeed());
+        this.setPlayButton('Стоп');
+    },
+
+    stop() {
+        this.status.setStopped();
+        clearInterval(this.tickInterval);
+        this.setPlayButton('Старт');
+    },
+
+    finish() {
+        this.status.setFinished();
+        clearInterval(this.tickInterval);
+        this.score.render()
+        this.setPlayButton('Игра закончена', true);
+    },
+
+    setPlayButton(text, isDisabled = false) {
+        const playButton = document.getElementById('playButton');
+
+        playButton.textContent = text;
+        isDisabled
+            ? playButton.classList.add('disabled')
+            : playButton.classList.remove('disabled');
+    },
+
+    tickHandler() {
+        if (!this.canMakeStep()) return this.finish();
+
+        if (this.food.isOnPoint(this.snake.getNextStepHeadPoint())) {
+            this.snake.growUp();
+            this.food.setCoordinates(this.getRandomFreeCoordinates());
+            this.barrier.setCoordinates(this.getRandomFreeCoordinates());
+
+            if (this.isGameWon()) return this.finish();
+        }
+
+        this.snake.makeStep();
+        this.score.setTotal(this.snake.body.length - 1)
+        this.render();
+    },
+
+    getStartSnakeBody() {
+        return [
+            {
+                x: Math.floor(this.config.getColsCount() / 2),
+                y: Math.floor(this.config.getRowsCount() / 2),
+            }
+        ];
+    },
+
+    setEventHandlers() {
+        document.getElementById('playButton').addEventListener('click', () => {
+            this.playClickHandler();
+        });
+
+        document.getElementById('newGameButton').addEventListener('click', () => {
+            this.newGameClickHandler();
+        });
+
+        document.addEventListener('keydown', (event) => this.keyDownHandler(event));
+    },
+
+    getRandomFreeCoordinates() {
+        const exclude = [this.food.getCoordinates(), this.barrier.getCoordinates(), ...this.snake.getBody()];
+
+        while (true) {
+            const rndPoint = {
+                x: Math.floor(Math.random() * this.config.getColsCount()),
+                y: Math.floor(Math.random() * this.config.getRowsCount()),
+            };
+
+            if (!exclude.some(exPoint => rndPoint.x === exPoint.x && exPoint.y === exPoint.y)) return rndPoint;
+        }
+    },
+
+    render() {
+        this.map.render(this.snake.getBody(), this.food.getCoordinates(), this.barrier.getCoordinates());
+        this.score.render()
+    },
+
+    canMakeStep() {
+        const nextHeadPoint = this.snake.getNextStepHeadPoint();
+
+        return !this.snake.isOnPoint(nextHeadPoint) &&
+            !this.barrier.isOnPoint(nextHeadPoint) &&
+            nextHeadPoint.x < this.config.getColsCount() &&
+            nextHeadPoint.y < this.config.getRowsCount() &&
+            nextHeadPoint.x >= 0 &&
+            nextHeadPoint.y >= 0;
+    },
+
+    isGameWon() {
+        return this.snake.getBody().length > this.config.getWinFoodCount();
+    },
+
+    playClickHandler() {
+        if (this.status.isPlaying()) {
+            this.stop();
+        } else if (this.status.isStopped()) {
+            this.play();
+        }
+    },
+
+    newGameClickHandler() {
+        this.reset();
+    },
+
+    keyDownHandler(event) {
+        if (!this.status.isPlaying()) return;
+
+        const direction = this.getDirectionByCode(event.code);
+
+        if (this.canSetDirection(direction)) {
+            this.snake.setDirection(direction);
+        }
+    },
+
+    getDirectionByCode(code) {
+        switch (code) {
+            case 'KeyW':
+            case 'ArrowUp':
+                return 'up';
+            case 'KeyD':
+            case 'ArrowRight':
+                return 'right';
+            case 'KeyS':
+            case 'ArrowDown':
+                return 'down';
+            case 'KeyA':
+            case 'ArrowLeft':
+                return 'left';
+            default:
+                return '';
+        }
+    },
+
+    canSetDirection(direction) {
+        const lastStepDirection = this.snake.getLastStepDirection();
+
+        return direction === 'up' && lastStepDirection !== 'down' ||
+            direction === 'right' && lastStepDirection !== 'left' ||
+            direction === 'down' && lastStepDirection !== 'up' ||
+            direction === 'left' && lastStepDirection !== 'right';
+    },
+};
+
+
+game.init();
